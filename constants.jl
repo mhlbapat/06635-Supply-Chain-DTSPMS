4edcb9296fd6d4ec6d1ba8af4060d663c811c1e8
Mehul Bapat <mhlbapat@gmail.com>
2021-07-07 03:36:34 -0400

    Made small changes to init_sol_gen

---
+++ constants.jl
@@ -0,0 +1,2 @@
+stack_capacity = Int64(11)
+nstacks = Int64(3)
--- functions/initial_solution_generator.jl
+++ functions/initial_solution_generator.jl
@@ -1,3 +1,25 @@
-function f(x,y)
-x+y
+import Random
+
+cd("..")
+include("../constants.jl")
+
+
+function initial_solution_generator(pickup_nodes, delivery_nodes)
+#= 
+Input: pickup, delivery arrays
+
+Output: Randomized initial solution returning pickup_route, delivery_route and stack assignment
+=#
+	random_initial_solution_pickup = Random.shuffle(pickup_nodes)
+	stack_assignment = [[] for i in 1:nstacks]
+	[append!(stack_assignment[mod(i, nstacks)+1], random_initial_solution_pickup[i]) for i in 1:length(random_initial_solution_pickup)]
+
+	println(random_initial_solution_pickup, "   ",stack_assignment)
+	println(stack_assignment[3])
+
+
+
 end
+
+
+initial_solution_generator([1,2,3,4,5,10,111,12], [1,2,3,4,5])
