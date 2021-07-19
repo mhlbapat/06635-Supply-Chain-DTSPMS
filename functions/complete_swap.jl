e918e89f0db2c500db92d071d5d9da9611383c8d
Mehul Bapat <mhlbapat@gmail.com>
2021-07-18 23:19:38 -0400

    Added complete swap code

--- functions/complete_swap.jl
+++ functions/complete_swap.jl
@@ -17,6 +17,23 @@ function complete_swap(pickup_route, delivery_route, stack_assignment)
 
 	for i in 1:length_route
 		for j in i:length_route
-			
+
+			temp_stack_assignment = deepcopy(stack_assignment)
+			temp_pickup_route = deepcopy(pickup_route)
+			temp_delivery_route = deepcopy(delivery_route)
+
+			node1 = temp_pickup_route[i]
+			node2 = temp_pickup_route[j]
+
+			if any([(node1 in temp_stack_assignment[j1]) && (node2 in temp_stack_assignment[j1]) for j1 in 1:nstacks])==false
+
+				# Replace in stack assignment
+				replace.(temp_stack_assignment, node1=>node2,node2=>node1)
+
+				# Replace in pickup route
+				replace!(temp_pickup_route, node1=>node2, node2=>node1) 
+			end
+
+
 		end
 	end
--- testing_code.jl
+++ testing_code.jl
@@ -4,13 +4,16 @@ include("functions/initial_solution_generator.jl")
 include("functions/route_swap.jl")
 
 
-pickupnodes, deliverynodes, stacks = initial_solution_generator(33)
+# pickupnodes, deliverynodes, stacks = initial_solution_generator(33)
 
+if any([false, false, false])==false
+	println("helloworld")
+end
 # println(stacks)
 
 # route_swap_neighborhood = route_swap(pickupnodes,deliverynodes,stacks)
 # println(size(route_swap_neighborhood))
-println(pickupnodes)
-println(deliverynodes)
-println([length(stacks[i]) for i in 1:nstacks])
+# println(pickupnodes)
+# println(deliverynodes)
+# println([length(stacks[i]) for i in 1:nstacks])
 # println(route_swap_neighborhood)
